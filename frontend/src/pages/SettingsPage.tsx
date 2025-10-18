e5f62a9f065db09d1f1b5f4d75aa79d4322c67fb
Hirotoshi Uchida <uho02741358@gmail.com>
2025-10-18 21:54:26 +0900

    Create SettingsPage.tsx

---
+++ frontend/src/pages/SettingsPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const SettingsPage: React.FC = () => (
+<div style={{ padding: 24 }}>
+<h1>Settings</h1>
+<p>Theme, language and account settings go here.</p>
+</div>
+);
+
+
+export default SettingsPage;
