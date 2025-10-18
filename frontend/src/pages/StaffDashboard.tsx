83f25581be7cba120f48327b6e60d854ad9f1f1d
Hirotoshi Uchida <uho02741358@gmail.com>
2025-10-18 21:53:11 +0900

    Create StaffDashboard.tsx

---
+++ frontend/src/pages/StaffDashboard.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const StaffDashboard: React.FC = () => (
+<div style={{ padding: 24 }}>
+<h1>Staff Dashboard</h1>
+<p>Welcome, staff. Replace this with real staff UI.</p>
+</div>
+);
+
+
+export default StaffDashboard;
