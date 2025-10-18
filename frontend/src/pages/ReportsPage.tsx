b6bdbe2468de5a62a376ef97e9aef3edb1a18e0d
Hirotoshi Uchida <uho02741358@gmail.com>
2025-10-18 21:54:01 +0900

    Create ReportsPage.tsx

---
+++ frontend/src/pages/ReportsPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const ReportsPage: React.FC = () => (
+<div style={{ padding: 24 }}>
+<h1>Reports</h1>
+<p>Reports and charts will be here.</p>
+</div>
+);
+
+
+export default ReportsPage;
