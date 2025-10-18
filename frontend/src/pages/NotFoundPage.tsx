f8f3d9e0d1889410ff6cfbf26b7d681cf3668614
Hirotoshi Uchida <uho02741358@gmail.com>
2025-10-18 21:55:24 +0900

    Create NotFoundPage.tsx

---
+++ frontend/src/pages/NotFoundPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const NotFoundPage: React.FC = () => (
+<div style={{ padding: 24 }}>
+<h1>404 - Not Found</h1>
+<p>The page you requested was not found.</p>
+</div>
+);
+
+
+export default NotFoundPage;
